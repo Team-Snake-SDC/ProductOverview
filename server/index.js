e076285a80b3448b9cbeef3051d6952224449591
Andrew Fuller <fuller2580@gmail.com>
2022-11-09 17:01:05 -0500

    loaderIO

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 3001;
@@ -9,6 +10,9 @@ app.get('/', (req, res) => {
   res.send({info: 'node.js, Express, and Postgres'});
 })
 
+app.get('/'+process.env.LOADIOKEY, (req, res) => {
+  res.send(process.env.LOADIOKEY);
+})
 app.get('/products', queries.getAllProducts);
 app.get('/products/:product_id', queries.getProductById);
 app.get('/products/:product_id/styles', queries.getProductStyles);
