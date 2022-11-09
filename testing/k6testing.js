9cdf1fff8e4b6c8d5d89ad921faa6c150323c94c
Andrew Fuller <fuller2580@gmail.com>
2022-11-09 14:36:40 -0500

    k6testing

--- testing/k6testing.js
+++ testing/k6testing.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep, check, group } from 'k6';
 
 export const options = {
-  vus: 1000, // Virtual Users
+  vus: 50, // Virtual Users
   duration: '20s'
 };
 
-const allProducts = `http://localhost:3001/products?page=${Math.floor(Math.random() * 200)}&count=${Math.floor(Math.random() * 100)}`;
+const allProducts = `http://localhost:3001/products?page=${Math.abs(Math.floor(Math.random() * 200))}&count=${Math.abs(Math.floor(Math.random() * 100))}`;
 const product = `http://localhost:3001/products/${Math.floor(Math.random() * 1000000) + 1}`;
 const styles = `http://localhost:3001/products/${Math.floor(Math.random() * 1000000) + 1}/styles`;
 const related = `http://localhost:3001/products/${Math.floor(Math.random() * 1000000) + 1}/related`;
@@ -18,55 +18,55 @@ export default function test() {
     check(allProductsResponse, {
     'transaction time 10ms': (r) => r.timings.duration < 10,
     'transaction time 50ms': (r) => r.timings.duration < 50,
+    'transaction time 100ms': (r) => r.timings.duration < 100,
+    'transaction time 150ms': (r) => r.timings.duration < 150,
     'transaction time 200ms': (r) => r.timings.duration < 200,
+    'transaction time 250ms': (r) => r.timings.duration < 250,
     'transaction time 500ms': (r) => r.timings.duration < 500,
-    'transaction time 1000ms': (r) => r.timings.duration < 1000,
-    'transaction time 2000ms': (r) => r.timings.duration < 2000,
-    'transaction time 5000ms': (r) => r.timings.duration < 5000,
-    'transaction time 10s': (r) => r.timings.duration < 10000,
-    'transaction time 20s': (r) => r.timings.duration < 20000,
+    'transaction time 1s': (r) => r.timings.duration < 1000,
+    'transaction time 2s': (r) => r.timings.duration < 2000,
     });
   });
-  group('getProduct', () => {
+  group('getProductById', () => {
     const getProductResponse = http.get(product);
     check(getProductResponse, {
       'transaction time 10ms': (r) => r.timings.duration < 10,
       'transaction time 50ms': (r) => r.timings.duration < 50,
+      'transaction time 100ms': (r) => r.timings.duration < 100,
+      'transaction time 150ms': (r) => r.timings.duration < 150,
       'transaction time 200ms': (r) => r.timings.duration < 200,
+      'transaction time 250ms': (r) => r.timings.duration < 250,
       'transaction time 500ms': (r) => r.timings.duration < 500,
-      'transaction time 1000ms': (r) => r.timings.duration < 1000,
-      'transaction time 2000ms': (r) => r.timings.duration < 2000,
-      'transaction time 5000ms': (r) => r.timings.duration < 5000,
-      'transaction time 10s': (r) => r.timings.duration < 10000,
-      'transaction time 20s': (r) => r.timings.duration < 20000,
+      'transaction time 1s': (r) => r.timings.duration < 1000,
+      'transaction time 2s': (r) => r.timings.duration < 2000,
     });
   });
-  group('getStyles', () => {
+  group('getProductStyles', () => {
     const stylesResponse = http.get(styles);
     check(stylesResponse, {
-    'transaction time 10ms': (r) => r.timings.duration < 10,
-    'transaction time 50ms': (r) => r.timings.duration < 50,
-    'transaction time 200ms': (r) => r.timings.duration < 200,
-    'transaction time 500ms': (r) => r.timings.duration < 500,
-    'transaction time 1000ms': (r) => r.timings.duration < 1000,
-    'transaction time 2000ms': (r) => r.timings.duration < 2000,
-    'transaction time 5000ms': (r) => r.timings.duration < 5000,
-    'transaction time 10s': (r) => r.timings.duration < 10000,
-    'transaction time 20s': (r) => r.timings.duration < 20000,
+      'transaction time 10ms': (r) => r.timings.duration < 10,
+      'transaction time 50ms': (r) => r.timings.duration < 50,
+      'transaction time 100ms': (r) => r.timings.duration < 100,
+      'transaction time 150ms': (r) => r.timings.duration < 150,
+      'transaction time 200ms': (r) => r.timings.duration < 200,
+      'transaction time 250ms': (r) => r.timings.duration < 250,
+      'transaction time 500ms': (r) => r.timings.duration < 500,
+      'transaction time 1s': (r) => r.timings.duration < 1000,
+      'transaction time 2s': (r) => r.timings.duration < 2000,
     });
   });
-  group('related', () => {
+  group('getRelatedItems', () => {
     const relatedResponse = http.get(related);
     check(relatedResponse, {
       'transaction time 10ms': (r) => r.timings.duration < 10,
       'transaction time 50ms': (r) => r.timings.duration < 50,
+      'transaction time 100ms': (r) => r.timings.duration < 100,
+      'transaction time 150ms': (r) => r.timings.duration < 150,
       'transaction time 200ms': (r) => r.timings.duration < 200,
+      'transaction time 250ms': (r) => r.timings.duration < 250,
       'transaction time 500ms': (r) => r.timings.duration < 500,
-      'transaction time 1000ms': (r) => r.timings.duration < 1000,
-      'transaction time 2000ms': (r) => r.timings.duration < 2000,
-      'transaction time 5000ms': (r) => r.timings.duration < 5000,
-      'transaction time 10s': (r) => r.timings.duration < 10000,
-      'transaction time 20s': (r) => r.timings.duration < 20000,
+      'transaction time 1s': (r) => r.timings.duration < 1000,
+      'transaction time 2s': (r) => r.timings.duration < 2000,
     });
   });
 }
